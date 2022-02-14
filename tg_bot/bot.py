0b68931c034049983a15aa9e3c517b134568eeed
alexandr310701@gmail.com <alexandr310701@gmail.com>
2022-02-14 14:47:45 +0200

    +- norm

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -6,6 +6,15 @@ bot = telebot.TeleBot('5152696792:AAFcyi0XKhr-Ma13TC3UvkPUEwsW6JUkJtg')
 def start(m, res=False):
     bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
 
+bot.message_handler(content_types=["text"])
+def any_msg(message):
+    keyboard = types.InlineKeyboardMarkup()
+    callback_button = types.InlineKeyboardButton(text="Нажми меня", callback_data="test")
+    keyboard.add(callback_button)
+    bot.send_message(message.chat.id, "Я – сообщение из обычного режима", reply_markup=keyboard)
+
+
+
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
